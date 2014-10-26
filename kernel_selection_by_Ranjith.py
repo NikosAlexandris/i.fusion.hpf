1ffb19744c087298b2c160bd67b6f6b267084b06
Nikos Alexandris <nik@nikosalexandris.net>
2014-10-26 22:52:56 +0200

    Various Kernel Selection functions. Function for random ratio generation.

--- kernel_selection_by_Ranjith.py
+++ kernel_selection_by_Ranjith.py
@@ -1,15 +1,29 @@
 #
 # Function to select kernel size depending of resolution ratio
+# Trikala, October 2014
 #
 
-# provided by Ranjith, 26 Oct. 2014-------------------------------------------
-  # <https://class.coursera.org/interactivepython-005/forum/profile?user_id=9361576>
+# provided by Ranjith, 26 Oct. 2014 ------------------------------------------
+# <https://class.coursera.org/interactivepython-005/forum/profile?user_id=9361576>
 # ----------------------------------------------------------------------------
 
-# requires librairies
+# modified by Anonymous ------------------------------------------------------
+# ?
+# ----------------------------------------------------------------------------
+
+# discussions at:
+# <https://class.coursera.org/interactivepython-005/forum/thread?thread_id=3786>
+
+
+# requires librairies ========================================================
 import random
 
-# Resolution Ratio
+
+#
+# CONSTANTS ==================================================================
+#
+
+# Resolution Ratios
 RATIO_RANGES = ((1, 2.5),\
           (2.5, 3.5),\
           (3.5, 5.5),\
@@ -23,20 +37,27 @@ KERNEL_SIZES = (5, 7, 9, 11, 13, 15)
 # zip'em
 MATRIX_PROPERTIES = zip ( RATIO_RANGES, KERNEL_SIZES )
 
+
+#
+# helper functions ===========================================================
+#
+
 ### Simulate user input ###
-ratio_low = min(RATIO_RANGES[0]) #; print ratio_low
-ratio_high = min(RATIO_RANGES[-1]) + 0.5 #; print ratio_high
-ratio_width = ratio_high - ratio_low #; print ratio_width
-ratio_random = round ( random.random() * ratio_width + ratio_low, 1)
-print "Ratio (Random):", ratio_random
+def ratio_random():
+    ratio_low = min(RATIO_RANGES[0]) #; print ratio_low
+    ratio_high = min(RATIO_RANGES[-1]) + 0.5 #; print ratio_high
+    ratio_width = ratio_high - ratio_low #; print ratio_width
+    ratio_random = round ( random.random() * ratio_width + ratio_low, 1)
+    return ratio_random ; print "Ratio (Random):", ratio_random
 
 # function to select kernel size
 def kernel_selection_R(ratio_random):
     kernel_size = 0
-    for l in MATRIX_PROPERTIES:
-        if l[0][0] <= ratio_random < l[0][1]:
-            kernel_size = l[1]
+    return [int(k) for ((lo,hi),k) in MATRIX_PROPERTIES if lo <= ratio_random < hi]
     print "Kernel size set to", str(kernel_size)
 
+
+#
 # demo
+#
 kernel_selection_R(ratio_random)
